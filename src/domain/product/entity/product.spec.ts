ac315a3aa81a10d68c5f9848748730d5360c3617
freitaslucas <lucasfreitass10@live.com>
2024-02-14 11:31:23 -0300

    Notification Error Products

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -16,7 +16,13 @@ describe("Product unit tests", () => {
   it("should throw error when price is less than zero", () => {
     expect(() => {
       const product = new Product("123", "Name", -1);
-    }).toThrowError("Price must be greater than zero");
+    }).toThrowError("product: Price must be greater than zero");
+  });
+
+  it("should throws errors when name is empty and price is less than zero", () => {
+    expect(() => {
+      const product = new Product("123", "", -1);
+    }).toThrowError("product: Name is required,product: Price must be greater than zero");
   });
 
   it("should change name", () => {
